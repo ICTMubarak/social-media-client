1f348b38d30dd2d914306a45479acc3e4da868db
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-12-27 09:49:12 -0800

    Added navbar

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOwPNN66qhc8wzEttcXcm4C_RAwu73uMo",
+  authDomain: "social-media-99d5b.firebaseapp.com",
+  projectId: "social-media-99d5b",
+  storageBucket: "social-media-99d5b.appspot.com",
+  messagingSenderId: "415841734428",
+  appId: "1:415841734428:web:8abf33dccd618051f9f4d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
