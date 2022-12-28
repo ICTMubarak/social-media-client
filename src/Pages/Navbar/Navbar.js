64a9e0f3bc3cd088a0fa5913e9e164e21b635523
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-12-27 18:17:30 -0800

    Update navbar

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -21,20 +21,8 @@ const Navbar = () => {
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
               <li><Link to='/'>Home</Link></li>
               
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Select Catagory
-                  <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                </a>
-                <ul className="p-2">
-                  <li><Link to="/hp">HP Laptop</Link></li>
-                  <li><Link to="/dell">Dell Laptop</Link></li>
-                  <li><Link to="/acer">Acer Laptop</Link></li>
-                </ul>
-              </li>
+             
 
-            <li><Link to='/addproduct'>Add Product</Link></li>
-            <li><Link to='/blog'>Blog</Link></li>
 
             {
               user?.uid ?
@@ -47,16 +35,7 @@ const Navbar = () => {
             }
 
 
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Dashboard
-                  <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                </a>
-                <ul className="p-2">
-                  <li><Link to="/allsellers">All Sellers</Link></li>
-                  <li><Link to="/allbuyers">All Buyers</Link></li>
-                </ul>
-              </li>
+             
 
             </ul>
             
@@ -67,20 +46,9 @@ const Navbar = () => {
           <ul className="menu menu-horizontal p-0">
           <li><Link to='/'>Home</Link></li>
           
-            <li tabIndex={0}>
-              <a>
-                Select Catagory
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-              </a>
-              <ul className="p-2">
-                  <li><Link to="/hp">HP Laptop</Link></li>
-                  <li><Link to="/dell">Dell Laptop</Link></li>
-                  <li><Link to="/acer">Acer Laptop</Link></li>
-              </ul>
-            </li>
+           
 
-          <li><Link to='/addproduct'>Add Product</Link></li>
-          <li><Link to='/blog'>Blog</Link></li>
+        
 
             {
               user?.uid ?
@@ -92,16 +60,7 @@ const Navbar = () => {
             
             }
 
-<li tabIndex={0}>
-                <a className="justify-between">
-                  Dashboard
-                  <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                </a>
-                <ul className="p-2">
-                  <li><Link to="/allsellers">All Sellers</Link></li>
-                  <li><Link to="/allbuyers">All Buyers</Link></li>
-                </ul>
-              </li>
+
             
           </ul>
         </div>
